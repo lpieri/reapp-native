946ae956c094dca3bdb562c868b134053fcc4f1c
Louise P <cpieri@student.42.fr>
2022-06-19 14:47:44 +0200

    [Update]: My first components

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   useColorScheme,
   View,
 } from 'react-native';
@@ -19,6 +9,8 @@ import {
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
+
+import TitleAtom from './srcs/components/atoms/TitleAtom'
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -29,7 +21,6 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
@@ -37,29 +28,11 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+			  <TitleAtom />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ srcs/components/atoms/TitleAtom.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class TitleAtom extends Component {
+	render () {
+		return <Text>Re-App Native</Text>;
+	}
+}
+
+export default TitleAtom;
