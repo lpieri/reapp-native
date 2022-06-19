94fb3b4674ff6be10828077e4df1e1ff04c00031
Louise P <cpieri@student.42.fr>
2022-06-19 15:11:37 +0200

    [Update]: Create font sizes

--- srcs/components/atoms/TitleAtom.js
+++ srcs/components/atoms/TitleAtom.js
@@ -2,9 +2,11 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 
+import fonts from '../../theme/fonts'
+
 class TitleAtom extends Component {
 	render () {
-		return <Text>Re-App Native</Text>;
+		return <Text style={{ fontSize: fonts.size.font20 }}>Re-App Native</Text>;
 	}
 }
 
